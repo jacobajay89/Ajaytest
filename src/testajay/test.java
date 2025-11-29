a9faa5cfff4a13e115768f16cbadec74eff0ac6e
jacobajay89 <jacobajay89@gmail.com>
2025-11-29 13:20:42 +0530

    Update test.java

--- src/testajay/test.java
+++ src/testajay/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Ajay test git");
 		System.out.println("Changes");
+		System.out.println("Edited in Github");
 
 	}
 
